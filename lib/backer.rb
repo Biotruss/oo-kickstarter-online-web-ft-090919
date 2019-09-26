4b3f530452f766456cba725525232b6f38de9a3a
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-26 00:22:41 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,14 @@
-clss 
+class Backer
+  attr_reader :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
+  end
 
 end
